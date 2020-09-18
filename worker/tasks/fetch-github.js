ae77b9e8049bde88980c0699d4824a97faf585f2
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2020-09-18 22:36:09 +0530

    commit on 'master'

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -2,7 +2,10 @@ var fetch = require('node-fetch');
 
 const baseURL = 'https://jobs.github.com/positions.json';
 
-export default async function fetchGithub() {
-    const jobs = await fetch(baseURL);
+module.exports = async function fetchGithub() {
+    const res = await fetch(baseURL);
+    const jobs = await res.json();
     console.log({ jobs });
+    console.log(jobs.length);
 }
+module.exports();
