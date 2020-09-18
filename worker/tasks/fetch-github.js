2038a1d6891de0d117aadd1f4dc84a29fefeb0ec
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2020-09-18 21:17:02 +0530

    commit

---
+++ worker/tasks/fetch-github.js
@@ -0,0 +1,7 @@
+var fetch = require('node-fetch');
+
+const baseURL = 'https://jobs.github.com/positions.json?description=ruby&page=1';
+
+export default async function fetchGithub() {
+    const jobs = await fetch()
+}
