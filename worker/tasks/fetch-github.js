d123685d97d884d27b8ec311fd5217288f307afc
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2020-09-18 22:16:31 +0530

    Update fetch-github.js

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -1,7 +1,8 @@
 var fetch = require('node-fetch');
 
-const baseURL = 'https://jobs.github.com/positions.json?description=ruby&page=1';
+const baseURL = 'https://jobs.github.com/positions.json';
 
 export default async function fetchGithub() {
-    const jobs = await fetch()
+    const jobs = await fetch(baseURL);
+    console.log({ jobs });
 }
