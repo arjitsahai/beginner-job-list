51fc74948366386048b498f1bd41f1928b32f069
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2020-09-20 16:00:10 +0530

    commit on 'master'

--- api/index.js
+++ api/index.js
@@ -11,9 +11,9 @@ const getAsync = promisify(client.get).bind(client);
 app.get('/jobs', async(req, res) => {
     const jobs = await getAsync('github');
 
-    console.log(JSON.parse(jobs).length);
 
-    return res.send('Hello World!')
+
+    return res.send(jobs);
 })
 
 app.listen(port, () => {
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
+import fetch from 'node-fetch';
 import React from 'react';
 import './App.css';
 import Jobs from './Jobs';
+
+const JOB_API_URL = 'http://localhost:3001';
 
 const mockJobs = [
     { title: 'SE 1', company: 'Microsoft' },
@@ -8,7 +11,23 @@ const mockJobs = [
     { title: 'SE 3', company: 'Facebook' }
 ]
 
+
+async function fetchjobs() {
+    const res = await fetch(JOB_API_URL);
+    const beginnerJobs = await res.json();
+
+
+    console.log({ beginnerJobs });
+}
+
 function App() {
+
+    React.useEffect(() => {
+        fetchjobs();
+    })
+
+
+
     return ( <
         div className = "App" >
         <
