84fb7994ace01dc2443c9f5091efb4905c9043db
arjitsahai <62187739+arjitsahai@users.noreply.github.com>
2020-09-26 01:23:50 +0530

    commit on 'master'

--- api/index.js
+++ api/index.js
@@ -1,19 +1,17 @@
 const express = require('express')
-const app = express();
-const port = 3001;
+const app = express()
+const port = 3001
 
-const redis = require("redis");
-const client = redis.createClient();
+var redis = require("redis"),
+    client = redis.createClient();
 
 const { promisify } = require('util');
 const getAsync = promisify(client.get).bind(client);
 
-app.get('/jobs', async(req, res) => {
+app.get('/api/jobs', async(req, res) => {
     const jobs = await getAsync('github');
     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    return res.send(jobs);
+    return res.send(jobs)
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at ${port}!`)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,30 @@
-import fetch from 'node-fetch';
 import React from 'react';
 import './App.css';
+
 import Jobs from './Jobs';
 
-const JOB_API_URL = 'http://localhost:3001';
+const JOB_API_URL = '/api/jobs';
 
-const mockJobs = [
-    { title: 'SE 1', company: 'Microsoft' },
-    { title: 'SE 2', company: 'Google' },
-    { title: 'SE 3', company: 'Facebook' }
-]
-
-
-async function fetchjobs(updateCb) {
+async function fetchJobs(updateCb) {
     const res = await fetch(JOB_API_URL);
-    const beginnerJobs = await res.json();
-    updateCb(beginnerJobs);
+    let json = await res.json();
 
-    console.log({ beginnerJobs });
+    updateCb(json);
 }
 
 function App() {
 
     const [jobList, updateJobs] = React.useState([]);
 
-
-
     React.useEffect(() => {
-        fetchjobs(updateJobs);
-    })
-
-
+        fetchJobs(updateJobs);
+    }, [])
 
     return ( <
         div className = "App" >
         <
         Jobs jobs = { jobList }
-        / >
-
-        <
+        /> <
         /div>
     );
 }
